d8020a938018df03e7a2d6a1a04f22c321d64690
Michael Lazarski <michael.lazarski@quantilope.com>
2018-12-04 09:23:58 +0200

    add social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,10 +6,24 @@ const Footer = styled.div`
   color: #F7F7F7;
   height: 50px;
   margin-top: -50px;
+  padding-right: 15px;
+  padding-left: 15px;
+
+  a {
+    float: right;
+  }
 `
 
 export default () => (
   <Footer>
-    <span class="brandico-twitter"></span>
+    <a href="https://www.instagram.com/lampewebdev/" target="_blank" >
+      <span class="zocial-instagram"></span>
+    </a>
+    <a href="https://github.com/lampewebdev" target="_blank" >
+      <span class="zocial-github"></span>
+    </a>
+    <a href="https://twitter.com/lampewebdev" target="_blank">
+      <span class="zocial-twitter"></span>
+    </a>
   </Footer>
 )
